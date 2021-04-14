4cce50f6ce125e5f01540cca4b157c7bafc6d49a
Murat Kudainetov <murat.kudainetov@gmail.com>
2021-04-14 08:27:02 +0300

    добавил слайдер карточек на первом экране страницы контекста

--- assets/templates/ms-template/js/context.js
+++ assets/templates/ms-template/js/context.js
@@ -1,37 +1,32 @@
 'use strict';
 $(document).ready(function () {
     $('#first').css('height', document.documentElement.clientHeight);
-    $('#second').css('height', document.documentElement.clientHeight);
-    $('#third').css('height', document.documentElement.clientHeight);
 
     let firstYMarker = 1 / 4 * $('#scroll-context').outerHeight();
     let secondYMarker = 2 / 4 * $('#scroll-context').outerHeight();
+
+    var cardslider = $('.my-cardslider').cardslider({
+        nav: false,
+        loop: false,
+        dots: false,
+        direction:'up'
+    }).data('cardslider');
 
     $(window).scroll(function () {
         if ((window.pageYOffset + document.documentElement.clientHeight) >= $('#scroll-context').outerHeight()) {
             console.log('more');
             $('#first').removeClass('fixed');
-            $('#second').removeClass('fixed');
-            $('#third').removeClass('fixed');
         } else {
             console.log('less');
             $('#first').addClass('fixed');
-            $('#second').addClass('fixed');
-            $('#third').addClass('fixed');
         }
 
         if (window.pageYOffset <= firstYMarker) {
-            $('#first').fadeIn(300);
-            $('#second').fadeOut(300);
-            $('#third').fadeOut(300);
+            cardslider.changeCardTo(0);
         } else if (window.pageYOffset > firstYMarker && window.pageYOffset <= secondYMarker) {
-            $('#first').fadeOut(300);
-            $('#second').fadeIn(300);
-            $('#third').fadeOut(300);
+            cardslider.changeCardTo(1);
         } else if (window.pageYOffset > secondYMarker) {
-            $('#first').fadeOut(300);
-            $('#second').fadeOut(300);
-            $('#third').fadeIn(300);
+            cardslider.changeCardTo(2);
         }
     });
 });
