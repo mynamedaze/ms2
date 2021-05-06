be36eeffe768fe29928e743a812bbdce507e6a03
Murat Kudainetov <murat.kudainetov@gmail.com>
2021-05-06 18:39:52 +0300

    верстаю второй шаг калькуляторва

--- assets/templates/ms-template/js/context.js
+++ assets/templates/ms-template/js/context.js
@@ -134,4 +134,20 @@ $(document).ready(function () {
         }
     });
     /*/team*/
+    /*calculator*/
+    //scroll
+    $( function() {
+        var handle = $( "#custom-handle" );
+        $( "#slider" ).slider({
+            min: 0,
+            max: 200000,
+            create: function() {
+                handle.text( $( this ).slider( "value" ) );
+            },
+            slide: function( event, ui ) {
+                handle.text( ui.value );
+            }
+        });
+    } );
+    /*/calculator*/
 });
