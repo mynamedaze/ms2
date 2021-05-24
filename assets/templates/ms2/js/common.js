dcb2297a98c486ad7b283e60f8dee2e0e9af8144
Murat Kudainetov <murat.kudainetov@gmail.com>
2021-05-24 14:05:32 +0300

    начал делать блог

---
+++ assets/templates/ms2/js/common.js
@@ -0,0 +1,163 @@
+'use strict';
+$(document).ready(function () {
+
+    let overlay = document.getElementsByClassName('overlay');
+    let popup = document.getElementsByClassName('popup');
+    let popupSuccess = document.getElementsByClassName('success-popup');
+    let closeBtn = document.getElementsByClassName('close-btn');
+
+    let navBurger = document.getElementsByClassName('page-header__nav-burger');
+    let mainNav = document.getElementsByClassName('main-nav');
+
+    let callbackBtn = document.getElementsByClassName('callback-btn');
+    let popupCallback = document.getElementsByClassName('callback-popup');
+
+    let pageHeaderSecondary = document.getElementsByClassName('page-header--secondary');
+
+    $(callbackBtn).click(function () {
+        $(overlay).fadeIn(300);
+        setTimeout(function () {
+            $(popupCallback).fadeIn(300);
+        }, 300);
+    });
+    $('.main-nav__callback-btn').click(function () {
+        $(mainNav).removeClass('active');
+    });
+
+    $(overlay).click(function () {
+        $(popup).fadeOut(300);
+        $(mainNav).removeClass('active');
+        setTimeout(function () {
+            $(overlay).fadeOut(300);
+        }, 300);
+    });
+
+    $(closeBtn).click(function () {
+        $(popup).fadeOut(300);
+        $(mainNav).removeClass('active');
+        setTimeout(function () {
+            $(overlay).fadeOut(300);
+        }, 100);
+    });
+
+    /*nav burger mobile*/
+
+    $(navBurger).click(function () {
+        $(overlay).fadeIn(300);
+        $(mainNav).addClass('active');
+    });
+    /*/nav burger mobile*/
+
+    /*fixed menu*/
+    $(window).scroll(function () {
+        if ((window.pageYOffset) >= 200) {
+            $(pageHeaderSecondary).addClass('white');
+        } else {
+            $(pageHeaderSecondary).removeClass('white');
+        }
+
+    });
+    /*/fixed menu*/
+
+    /*validate phone*/
+    let validatePhone = document.getElementsByClassName('validate-phone');
+    validatePhone = Array.prototype.slice.call(validatePhone);
+    validatePhone.forEach(function (item, index) {
+        $(item).inputmask("+X (999) 999-9999", {
+            definitions: {
+                "X": {
+                    validator: "[7-9]",
+                }
+            },
+            oncomplete: function(){
+                $(this).val('+7' + $(this).val().substring(2));
+            }
+        });
+    });
+    /*/validate phone*/
+
+    /* popupCallbackForm */
+    let popupCallbackForm = $('#callback-popup-form');
+
+    let callbackPopupInputName = document.getElementById('callback-popup-input-name');
+    let callbackPopupInputPhone = document.getElementById('callback-popup-input-phone');
+
+    popupCallbackForm.submit(function (ev) {
+        $.ajax({
+            type: 'POST',
+            url: '/assets/mailphp/context/ms2-mail.php',
+            data: popupCallbackForm.serialize(),
+            success: function (data) {
+                $(callbackPopupInputName).val('');
+                $(callbackPopupInputPhone).val('');
+                $(popupCallback).fadeOut(300);
+                setTimeout(function () {
+                    $(popupSuccess).fadeIn(300);
+                }, 290);
+                yaCounter49417246.reachGoal('context_callback');
+                yaCounter49417246.reachGoal('common');
+            }
+        });
+        ev.preventDefault();
+    });
+    /* */
+    /*team*/
+    if ($('.team')) {
+        let contextTeamCarousel;
+        if (document.documentElement.clientWidth < 768) {
+            contextTeamCarousel = $('.team__list').owlCarousel(
+                {
+                    loop:false,
+                    dots: false,
+                    nav: true,
+                    autoWidth: true,
+                    items: 4,
+                    responsiveClass:true
+                }
+            );
+        }
+
+        $( window ).resize(function() {
+            if (document.documentElement.clientWidth < 768) {
+                contextTeamCarousel = $('.team__list').owlCarousel(
+                    {
+                        loop:false,
+                        dots: false,
+                        nav: true,
+                        autoWidth: true,
+                        items: 4,
+                        responsiveClass:true
+                    }
+                );
+            } else {
+                contextTeamCarousel.trigger('destroy.owl.carousel');
+            }
+        });
+    }
+    /*/team*/
+    /*feedback*/
+    if ($('.feedback')) {
+        let feedbackPopup = document.getElementsByClassName('feedback__popup');
+
+        let feedbackMoreButton = document.getElementsByClassName('feedback__more-button');
+        feedbackMoreButton = Array.prototype.slice.call(feedbackMoreButton);
+
+        let feedbackItemPopup = document.getElementsByClassName('feedback__item--popup');
+        feedbackItemPopup = Array.prototype.slice.call(feedbackItemPopup);
+
+        feedbackMoreButton.forEach(function (item,index) {
+            $(item).click(function () {
+                $(feedbackItemPopup).hide();
+                $(feedbackItemPopup[index]).show();
+                $(feedbackPopup).css("display", "flex")
+                    .hide()
+                    .fadeIn();
+                $(overlay).fadeIn(300);
+                setTimeout(function () {
+                    $(feedbackPopup).fadeIn(300);
+                }, 300);
+            });
+        });
+    }
+    /*/feedback*/
+});
--- assets/templates/ms2/js/context.js
+++ assets/templates/ms2/js/context.js
@@ -1,70 +1,9 @@
 'use strict';
 $(document).ready(function () {
-    /*validate phone*/
-    let validatePhone = document.getElementsByClassName('validate-phone');
-    validatePhone = Array.prototype.slice.call(validatePhone);
-    validatePhone.forEach(function (item, index) {
-        $(item).inputmask("+X (999) 999-9999", {
-            definitions: {
-                "X": {
-                    validator: "[7-9]",
-                }
-            },
-            oncomplete: function(){
-                $(this).val('+7' + $(this).val().substring(2));
-            }
-        });
-    });
-    /*/validate phone*/
 
-    let overlay = document.getElementsByClassName('overlay');
-    let popup = document.getElementsByClassName('popup');
-    let popupSuccess = document.getElementsByClassName('success-popup');
-    let closeBtn = document.getElementsByClassName('close-btn');
-
-    let navBurger = document.getElementsByClassName('page-header__nav-burger');
-    let mainNav = document.getElementsByClassName('main-nav');
-
-    let pageHeader = document.getElementsByClassName('page-header');
-
-    let callbackBtn = document.getElementsByClassName('callback-btn');
-    let popupCallback = document.getElementsByClassName('callback-popup');
+    let pageHeaderPrimary = document.getElementsByClassName('page-header--primary');
 
     let stage2DiscountPercent = document.getElementById('stage2-discount-percent');
-
-    $(callbackBtn).click(function () {
-        $(overlay).fadeIn(300);
-        setTimeout(function () {
-            $(popupCallback).fadeIn(300);
-        }, 300);
-    });
-    $('.main-nav__callback-btn').click(function () {
-        $(mainNav).removeClass('active');
-    });
-
-    $(overlay).click(function () {
-       $(popup).fadeOut(300);
-       $(mainNav).removeClass('active');
-       setTimeout(function () {
-           $(overlay).fadeOut(300);
-       }, 300);
-    });
-
-    $(closeBtn).click(function () {
-        $(popup).fadeOut(300);
-        $(mainNav).removeClass('active');
-        setTimeout(function () {
-            $(overlay).fadeOut(300);
-        }, 100);
-    });
-
-    /*nav burger mobile*/
-
-    $(navBurger).click(function () {
-        $(overlay).fadeIn(300);
-        $(mainNav).addClass('active');
-    });
-    /*/nav burger mobile*/
 
     let scrollContext = document.getElementById('scroll-context');
 
@@ -97,9 +36,9 @@ $(document).ready(function () {
             }
 
             if ((window.pageYOffset) + 50 >= $(scrollContext).outerHeight()) {
-                $(pageHeader).addClass('white');
+                $(pageHeaderPrimary).addClass('white');
             } else {
-                $(pageHeader).removeClass('white');
+                $(pageHeaderPrimary).removeClass('white');
             }
 
             if (window.pageYOffset <= firstYMarker) {
@@ -145,63 +84,6 @@ $(document).ready(function () {
             contextFeedbackCarousel.trigger('refreshed.owl.carousel');
         });
     }
-    /*feedback*/
-    if ($('.feedback')) {
-        let feedbackPopup = document.getElementsByClassName('feedback__popup');
-
-        let feedbackMoreButton = document.getElementsByClassName('feedback__more-button');
-        feedbackMoreButton = Array.prototype.slice.call(feedbackMoreButton);
-
-        let feedbackItemPopup = document.getElementsByClassName('feedback__item--popup');
-        feedbackItemPopup = Array.prototype.slice.call(feedbackItemPopup);
-
-        feedbackMoreButton.forEach(function (item,index) {
-           $(item).click(function () {
-               $(feedbackItemPopup).hide();
-               $(feedbackItemPopup[index]).show();
-               $(feedbackPopup).css("display", "flex")
-                   .hide()
-                   .fadeIn();
-               $(overlay).fadeIn(300);
-               setTimeout(function () {
-                   $(feedbackPopup).fadeIn(300);
-               }, 300);
-           });
-        });
-    }
-    /*/feedback*/
-    /*team*/
-    let contextTeamCarousel;
-    if (document.documentElement.clientWidth < 768) {
-        contextTeamCarousel = $('.team__list').owlCarousel(
-            {
-                loop:false,
-                dots: false,
-                nav: true,
-                autoWidth: true,
-                items: 4,
-                responsiveClass:true
-            }
-        );
-    }
-
-    $( window ).resize(function() {
-        if (document.documentElement.clientWidth < 768) {
-            contextTeamCarousel = $('.team__list').owlCarousel(
-                {
-                    loop:false,
-                    dots: false,
-                    nav: true,
-                    autoWidth: true,
-                    items: 4,
-                    responsiveClass:true
-                }
-            );
-        } else {
-            contextTeamCarousel.trigger('destroy.owl.carousel');
-        }
-    });
-    /*/team*/
 
     /*calculator*/
     let calculatorHeadItem = document.getElementsByClassName('calculator__head-item');
@@ -407,32 +289,6 @@ $(document).ready(function () {
         ev.preventDefault();
     });
     /*/calculator*/
-
-    /* popupCallbackForm */
-    let popupCallbackForm = $('#callback-popup-form');
-
-    let callbackPopupInputName = document.getElementById('callback-popup-input-name');
-    let callbackPopupInputPhone = document.getElementById('callback-popup-input-phone');
-
-    popupCallbackForm.submit(function (ev) {
-        $.ajax({
-            type: 'POST',
-            url: '/assets/mailphp/context/ms2-mail.php',
-            data: popupCallbackForm.serialize(),
-            success: function (data) {
-                $(callbackPopupInputName).val('');
-                $(callbackPopupInputPhone).val('');
-                $(popupCallback).fadeOut(300);
-                setTimeout(function () {
-                    $(popupSuccess).fadeIn(300);
-                }, 290);
-                yaCounter49417246.reachGoal('context_callback');
-                yaCounter49417246.reachGoal('common');
-            }
-        });
-        ev.preventDefault();
-    });
-    /* */
 
     /* supportForm */
     let supportForm = $('#support-form');
